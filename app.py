478fa4905745ceb13b3a46c535030e88e58d9895
JDLGC <dlgcjorge@gmail.com>
2024-01-14 04:50:59 +0100

    implementation(processing): transformed some values and prepared OneHotEncoder, VectorAssembler and Normalizer.

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from pathlib import Path
 from typing import Optional, List, Dict
 
 from pyspark import SparkConf
-from pyspark.ml.feature import MinMaxScaler, VectorAssembler
+from pyspark.ml.feature import MinMaxScaler, StringIndexer, OneHotEncoder, VectorAssembler, Normalizer
+from pyspark.ml.clustering import KMeans
+from pyspark.ml.pipeline import Pipeline
 from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.functions import col
+from pyspark.sql.functions import *
 from pyspark.sql.types import IntegerType, StringType, BooleanType, StructField, StructType
 
 # Constants
@@ -97,7 +99,48 @@ class SparkApp:
         return df_filtered
 
     def process_data(self, df: DataFrame) -> DataFrame:
-        # TODO: Feeding the data to the ML model
+        # Create a VectorAssembler
+        # Flights that have not been cancelled (cancelled != 0) and whose elapsed time is positive
+        # will be the ones used, as the goal is to predict the arrival time
+        df = df.filter((df["Cancelled"] != 0) & (df['CRSElapsedTime'] > 0)).distinct()
+
+        # DayofMonth, Month and Year could be an unique field called Date with 'dd/MM/yyyy' format
+        df = df.withColumn("Date", concat(col("DayofMonth"), lit("/"), col("Month"), lit("/"), col("Year"))).drop("DayofMonth").drop("Month").drop("Year")
+
+        # DayofWeek could be clearer if instead of using number it uses the names of the week
+        df = df.withColumn("DayofWeek", 
+                           when(col("DayofWeek") == 1, "Monday")
+                           .when(col("DayofWeek") == 2, "Tuesday")
+                           .when(col("DayofWeek") == 3, "Wednesday")
+                           .when(col("DayofWeek") == 4, "Thursday")
+                           .when(col("DayofWeek") == 5, "Friday")
+                           .when(col("DayofWeek") == 6, "Saturdya")
+                           .when(col("DayofWeek") == 7, "Sunday"))
+        
+        # DepTime, CRSDepTime, CRSArrTime could be clearer if insted of using a number or hour it uses Time of Day
+        # It is useful to prevent overfitting, enhance model interpretability and helpful for some models
+        df = df.withColumn("DepTime",
+                           when((col("DepTime") >= 1800) & (col("DepTime") <= 2359), "Evening")
+                           .when((col("DepTime") >= 1200) & (col("DepTime") < 1800), "Afternoon")
+                           .when((col("DepTime") >= 600) & (col("DepTime") < 1200), "Morning")
+                           .when((col("DepTime") >= 0) & (col("DepTime") < 600), "Overnight")
+                           .otherwise("Unknown"))
+        
+        df = df.withColumn("CRSDepTime",
+                    when((col("CRSDepTime") >= 1800) & (col("CRSDepTime") <= 2359), "Evening")
+                    .when((col("CRSDepTime") >= 1200) & (col("CRSDepTime") < 1800), "Afternoon")
+                    .when((col("CRSDepTime") >= 600) & (col("CRSDepTime") < 1200), "Morning")
+                    .when((col("CRSDepTime") >= 0) & (col("CRSDepTime") < 600), "Overnight")
+                    .otherwise("Unknown"))
+        
+        df = df.withColumn("CRSArrTime",
+                           when((col("CRSArrTime") >= 1800) & (col("CRSArrTime") <= 2359), "Evening")
+                           .when((col("CRSArrTime") >= 1200) & (col("CRSArrTime") < 1800), "Afternoon")
+                           .when((col("CRSArrTime") >= 600) & (col("CRSArrTime") < 1200), "Morning")
+                           .when((col("CRSArrTime") >= 0) & (col("CRSArrTime") < 600), "Overnight")
+                           .otherwise("Unknown"))
+        
+        # TODO: Missing values
         return df
 
     def run(self) -> None:
@@ -108,6 +151,38 @@ class SparkApp:
         df = self.process_data(df)
 
         # TODO: Testing some ML models
+        categorical_columns = ["DayofWeek", "DepTime", "CRSDepTime", "CRSDepTime", "UniqueCarrier", "Origin", "Dest"]
+        indexed_columns = [col + "_index" for col in categorical_columns]
+        encoded_columns = ["encoded_" + col for col in categorical_columns]
+        other_columns = ["Date", "FlightNum", "CRSElapsedTime", "DepDelay", "Distance", "Cancelled"]
+        assembler_columns = encoded_columns + other_columns
+        
+        # Create a StringIndexer to convert the categorical column to indices
+        indexers = [StringIndexer(inputCol=col, outputCol=indexed_columns) for col, indexed_col in categorical_columns]
+        # # Create a OneHotEncoder to encode the indices into one-hot vectors
+        # encoder = OneHotEncoder(inputCol=indexed_columns, outputCol=encoded_columns)
+        
+        # # Create a VectorAssembler
+        # assembler = VectorAssembler(inputCols=assembler_columns, outputCol="features")
+        # dataset = assembler.transform(df)
+
+        # # Create a Normalizer
+        # normalizer = Normalizer(inputCol="features", outputCol="normFeatures", p=1.0)
+
+        # # Create K-Means model
+        # kmeans = KMeans(featuresCol="features", k=2)
+
+        # # Create a Pipeline
+        # pipeline = Pipeline(stages=[assembler, normalizer, kmeans])
+
+        # # Fit the Pipeline on the training data
+        # model = pipeline.fit(training)
+
+        # # Transform the test data and show the results
+        # transformed_data = model.transform(test)
+        # transformed_data.show(truncate=False)
+
+        
 
         # TODO: Remove this
         df.show(10)
